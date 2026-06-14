ec7c1257164df2c8ab9024fcd90652e39b8832e3
BenArfanour <benarfanour9@gmail.com>
2026-06-14 05:14:03 +0200

    fix: document OPcache preload config for production (#73)

--- config/preload.php
+++ config/preload.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * OPcache preloading entry point (production only).
+ *
+ * Symfony generates var/cache/prod/App_KernelProdContainer.preload.php after:
+ *   APP_ENV=prod composer install --no-dev --optimize-autoloader
+ *   php bin/console cache:warmup --env=prod
+ *
+ * To enable in Docker, add to php opcache.ini (disabled in dev by default):
+ *   opcache.preload=/var/www/html/config/preload.php
+ *   opcache.preload_user=www-data
+ *
+ * @see https://symfony.com/doc/current/performance/opcache.html
+ */
 if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
     require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
 }
